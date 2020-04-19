e2e0a8f8ec96a580b17699afcfc888228e38104f
aditya vasudeva <aditya.vasudeva@nagarro.com>
2020-04-19 18:41:52 +0530

    Updated Author for Petclinic Application

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -22,7 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * PetClinic Spring Boot Application.
  *
- * @author Dave Syer
+ * @author Aditya Vasudeva
  *
  */
 @SpringBootApplication(proxyBeanMethods = false)
